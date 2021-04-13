c95a3becc85a350c0094c5a034c20db1ba846007
Eirikur Ragnarsson <43515589+Eikonomics@users.noreply.github.com>
2021-04-14 00:50:07 +0200

    Add files via upload

---
+++ ConnectToAPI.R
@@ -0,0 +1,12 @@
+ConnectToApi <- function() {
+  # Hér þarf að setja inn API dótið úr emailinu
+  api_key <- ""
+  api_secret <- ""
+  access_token <- ""
+  access_token_secret <- ""
+  setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+  
+  1
+  
+}
+
